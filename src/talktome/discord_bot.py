971e571c1b57966bef5770a8182259108e105cde
Dawid Biliński <dawid.bilinski.99@gmail.com>
2025-04-30 20:40:08 +0200

    setup logging

--- src/talktome/discord_bot.py
+++ src/talktome/discord_bot.py
@@ -1,9 +1,25 @@
+import logging
+import logging.handlers
 import os
 
 import discord
 from dotenv import load_dotenv
 
 load_dotenv()
+
+logger = logging.getLogger('discord')
+logger.setLevel(logging.DEBUG)
+
+handler = logging.handlers.RotatingFileHandler(
+    filename='discord.log',
+    encoding='utf-8',
+    maxBytes=32 * 1024 * 1024,  # 32 MiB
+    backupCount=5,  # Rotate through 5 files
+)
+dt_fmt = '%Y-%m-%d %H:%M:%S'
+formatter = logging.Formatter('[{asctime}] [{levelname:<8}] {name}: {message}', dt_fmt, style='{')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -22,4 +38,4 @@ async def on_message(message: discord.Message):
     if message.content.startswith('$hello'):
         await message.channel.send('Hello!')
 
-client.run(os.environ['DISCORD_APP_TOKEN'])
+client.run(os.environ['DISCORD_APP_TOKEN'], log_handler=None)
