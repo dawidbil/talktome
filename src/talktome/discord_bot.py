57aba96086af45e9a3d641d324008593530307dd
Dawid Biliński <dawid.bilinski.99@gmail.com>
2025-05-01 12:32:13 +0200

    split logging setup to separate file

--- src/talktome/discord_bot.py
+++ src/talktome/discord_bot.py
@@ -1,41 +1,32 @@
-import logging
-import logging.handlers
 import os
 
 import discord
 from dotenv import load_dotenv
 
+from .setup_logging import setup_logging
+
 load_dotenv()
 
-logger = logging.getLogger('discord')
-logger.setLevel(logging.DEBUG)
-
-handler = logging.handlers.RotatingFileHandler(
-    filename='discord.log',
-    encoding='utf-8',
-    maxBytes=32 * 1024 * 1024,  # 32 MiB
-    backupCount=5,  # Rotate through 5 files
-)
-dt_fmt = '%Y-%m-%d %H:%M:%S'
-formatter = logging.Formatter('[{asctime}] [{levelname:<8}] {name}: {message}', dt_fmt, style='{')
-handler.setFormatter(formatter)
-logger.addHandler(handler)
+logger = setup_logging()
 
 intents = discord.Intents.default()
 intents.message_content = True
 
 client = discord.Client(intents=intents)
 
+
 @client.event
 async def on_ready():
-    print(f'We have logged in as {client.user}')
+    print(f"We have logged in as {client.user}")
+
 
 @client.event
 async def on_message(message: discord.Message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
+    if message.content.startswith("$hello"):
+        await message.channel.send("Hello!")
 
-client.run(os.environ['DISCORD_APP_TOKEN'], log_handler=None)
+
+client.run(os.environ["DISCORD_APP_TOKEN"], log_handler=None)
