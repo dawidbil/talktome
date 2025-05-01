bd05ed3165c9d4f4c6d5867c9d7611e214938a23
Dawid Biliński <dawid.bilinski.99@gmail.com>
2025-05-01 16:19:57 +0200

    send user message to openai api

--- src/talktome/discord_bot.py
+++ src/talktome/discord_bot.py
@@ -1,13 +1,21 @@
 import os
+from typing import cast
 
 import discord
 from dotenv import load_dotenv
+from langchain.chat_models import init_chat_model
+from langchain.schema import HumanMessage
 
-from .setup_logging import setup_logging
+from talktome.setup_logging import setup_logging
 
 load_dotenv()
 
 logger = setup_logging()
+
+chat_model = init_chat_model(
+    model="gpt-4o-mini",
+    model_provider="openai",
+)
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -25,8 +33,14 @@ async def on_message(message: discord.Message):
     if message.author == client.user:
         return
 
-    if message.content.startswith("$hello"):
-        await message.channel.send("Hello!")
+    if message.content.startswith("!ela"):
+        response = await chat_model.ainvoke(
+            [
+                HumanMessage(content=message.content),
+            ]
+        )
+        content = cast(str, response.content)
+        await message.channel.send(content)
 
 
 client.run(os.environ["DISCORD_APP_TOKEN"], log_handler=None)
